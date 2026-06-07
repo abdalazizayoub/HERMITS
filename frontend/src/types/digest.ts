8fc10013dc1ea25bdac254299392e38aa2cdef77
FilipDolejsi <bivoj05@gmail.com>
2026-06-07 11:46:03 +0200

    no frontend

--- frontend/src/types/digest.ts
+++
@@ -1,7 +0,0 @@
-export interface MonthlyDigestResult {
-  transcript: string
-  total_tickets: number
-  avg_resolution_minutes: number
-  most_common_root_cause: string
-  top_incidents: string[]
-}
